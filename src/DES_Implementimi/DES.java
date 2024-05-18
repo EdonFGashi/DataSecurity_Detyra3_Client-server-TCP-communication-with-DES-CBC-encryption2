248a6ebe6bd46b496da276e54365153c81f04081
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-18 19:54:20 +0200

    krijimi i klases DES

---
+++ src/DES_Implementimi/DES.java
@@ -0,0 +1,24 @@
+package DES_Implementimi;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class DES {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+    public static void encryption(){
+
+    }
+
+    public static void decryption(){
+    }
+}
