a2ecc40789e151d38d4132aff5e8209cb51198ca
dreninimanaj <67548507+dreninimanaj@users.noreply.github.com>
2024-05-19 22:54:23 +0200

    WriteThread

---
+++ src/Client/WriteThread.java
@@ -0,0 +1,25 @@
+package Client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class WriteThread extends Thread{
+    private PrintWriter writer;
+    private Socket socket;
+    private ChatClient client;
+
+    public WriteThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+        } catch (IOException ex) {
+            System.out.println("Error getting output stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
