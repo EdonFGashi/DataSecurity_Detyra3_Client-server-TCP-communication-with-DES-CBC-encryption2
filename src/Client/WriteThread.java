a56e2495cfec4cbf20970c72104ed759ae966a70
elionmehaj <mehajelion@gmail.com>
2024-05-19 22:58:22 +0200

    metoda run tek writeThread

--- src/Client/WriteThread.java
+++ src/Client/WriteThread.java
@@ -1,9 +1,16 @@
 package Client;
 
+import java.io.Console;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+/*
+ * Ky thread eshte i pergjegjshem qe te lexoj mesazhin qe jep klienti
+ * dhe e dergon ate ne server
+ * Ekzekutohet ne nje infinit loop deri sa klienti e shkruan 'dil'
+ * */
 
 public class WriteThread extends Thread{
     private PrintWriter writer;
@@ -20,6 +27,32 @@ public class WriteThread extends Thread{
         } catch (IOException ex) {
             System.out.println("Error getting output stream: " + ex.getMessage());
             ex.printStackTrace();
+        }
+    }
+
+    public void run() {
+
+        Console console = System.console();
+
+        String userName = console.readLine("\nShkruani emrin tuaj: ");
+        client.setUserName(userName);
+        writer.println(userName);
+
+        String text;
+
+        do {
+            text = console.readLine();
+
+            String tekstiEnktipruar = DES.encryption(text, "Blera1234");
+            writer.println(tekstiEnktipruar);
+
+        } while (!text.equals("dil"));
+
+        try {
+            socket.close();
+        } catch (IOException ex) {
+
+            System.out.println("Error gjat shkrimit ne server: " + ex.getMessage());
         }
     }
 }
