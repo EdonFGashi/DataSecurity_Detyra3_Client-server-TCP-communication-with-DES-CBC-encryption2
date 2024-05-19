862e07e53c12a2806f30729fb36eac8acdbb7aae
EdonFGashi <edongashi2004@outlook.com>
2024-05-19 11:38:09 +0200

    Threadi per leximin e mesazheve te klienti

---
+++ src/Client/ReadThread.java
@@ -0,0 +1,26 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ReadThread {
+    private BufferedReader reader;
+    private Socket socket;
+    private ChatClient client;
+
+    public ReadThread(Socket socket, ChatClient client) {
+        this.socket = socket;
+        this.client = client;
+
+        try {
+            InputStream input = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(input));
+        } catch (IOException ex) {
+            System.out.println("Error getting input stream: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
