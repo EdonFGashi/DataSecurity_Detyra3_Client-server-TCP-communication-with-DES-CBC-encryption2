03ef3ee3bd081c7d15875664d7bcf5e45d63ca8c
elionmehaj <mehajelion@gmail.com>
2024-05-19 20:35:27 +0200

    try catch

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -11,3 +11,19 @@ public class ChatClient {
         this.port = port;
     }
 }
+
+public void execute() {
+    try {
+        Socket socket = new Socket(this.hostname, this.port);
+        System.out.println("Connected to the chat server");
+        (new ReadThread(socket, this)).start();
+        (new WriteThread(socket, this)).start();
+    } catch (UnknownHostException var2) {
+        UnknownHostException ex = var2;
+        System.out.println("Server not found: " + ex.getMessage());
+    } catch (IOException var3) {
+        IOException ex = var3;
+        System.out.println("I/O Error: " + ex.getMessage());
+    }
+
+}
